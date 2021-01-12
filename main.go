15695e68dae80af7659e797849fb71ca2f6d4f4f
Gerifield <gergely.radics1@ibm.com>
2021-01-12 15:50:49 +0100

    feat: configurable HTTP server address

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -9,14 +10,16 @@ import (
 )
 
 func main() {
+	listen := flag.String("listen", ":9090", "HTTP and WS server listen address")
+	flag.Parse()
 	r := chi.NewRouter()
 	e := epher.New()
 
 	r.Get("/subscribe/{room:[a-zA-Z0-9]+}", e.WebsocketHandler) // Websocket
 	r.Post("/publish/{room:[a-zA-Z0-9]+}", e.PushHandler)       // Http
 
-	log.Println("Started")
-	err := http.ListenAndServe("127.0.0.1:9090", r)
+	log.Printf("Started on %s\n", *listen)
+	err := http.ListenAndServe(*listen, r)
 	if err != nil {
 		log.Fatalln(err)
 	}
