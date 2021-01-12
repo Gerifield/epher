ecdfe46d709b8879577ef194075ab11a1130a9b8
Gerifield <gergely.radics1@ibm.com>
2021-01-12 20:42:59 +0100

    feat: added user writer tests

--- epher/user_test.go
+++ epher/user_test.go
@@ -1,6 +1,7 @@
 package epher
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"testing"
@@ -11,6 +12,10 @@ import (
 
 type testWsConn struct {
 	readMessageError error
+
+	nextWriterMessageType int
+	nextWriterStream      io.WriteCloser
+	nextWriterError       error
 }
 
 func (twc *testWsConn) ReadMessage() (messageType int, p []byte, err error) {
@@ -18,7 +23,8 @@ func (twc *testWsConn) ReadMessage() (messageType int, p []byte, err error) {
 }
 
 func (twc *testWsConn) NextWriter(messageType int) (io.WriteCloser, error) {
-	panic("implement me")
+	twc.nextWriterMessageType = messageType
+	return twc.nextWriterStream, twc.nextWriterError
 }
 
 func TestNewUser(t *testing.T) {
@@ -54,4 +60,33 @@ func TestReadLoopConnectionCleanClose(t *testing.T) {
 	u := NewUser(twc)
 
 	assert.NoError(t, u.ReadLoop())
+}
+
+func TestSendTextNextWriterFail(t *testing.T) {
+	testError := errors.New("something went wrong")
+	twc := &testWsConn{
+		nextWriterError: testError,
+	}
+	u := NewUser(twc)
+
+	assert.Equal(t, testError, u.SendText([]byte("test")))
+	assert.Equal(t, websocket.TextMessage, twc.nextWriterMessageType) // It was called
+}
+
+type buffCloser struct {
+	io.Writer
+}
+
+func (buffCloser) Close() error { return nil }
+
+func TestSendTextOK(t *testing.T) {
+	buff := &bytes.Buffer{}
+	twc := &testWsConn{
+		nextWriterStream: buffCloser{Writer: buff},
+	}
+	u := NewUser(twc)
+
+	assert.NoError(t, u.SendText([]byte("test")))
+	assert.Equal(t, websocket.TextMessage, twc.nextWriterMessageType)
+	assert.Equal(t, "test", buff.String())
 }
